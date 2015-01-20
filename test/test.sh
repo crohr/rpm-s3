f0ec8dca7446771fc44d8c9916857fb2bb9cd9bb
Cyril Rohr <cyril.rohr@gmail.com>
2015-01-20 00:46:54 +0000

    Generate repomd.xml.asc

--- test/test.sh
+++ test/test.sh
@@ -4,9 +4,15 @@ set -e
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
 ROOT_DIR="$(dirname "$DIR")"
+TMPDIR=$(mktemp -d)
+
+cp -r ${DIR}/.gnupg $TMPDIR/
+chmod 0700 $TMPDIR/.gnupg
+cp ${DIR}/.rpmmacros $TMPDIR/
 
 # used to get .rpmmacros and .gnupg
-HOME="$DIR"
+HOME="$TMPDIR"
+echo "HOME=$HOME"
 
 $ROOT_DIR/bin/rpm-s3 -b ${BUCKET:="pkgr-development-rpm"} -v -p gh/crohr/test/centos6/master --sign --keep 1000 ${DIR}/*.rpm
 
